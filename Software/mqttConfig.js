b47badc3f9ae4c60030664cc5dfc220ef065b63b
Rafael Gomes <rafael.gomes@axabr.ad>
2020-03-19 10:36:01 -0300

    Software improvements

--- Software/mqttConfig.js
+++ Software/mqttConfig.js
@@ -63,12 +63,3 @@ const MQTT = {
         mqttClient.send(message);
     }
 }
-
-// MQTT.connect("brware.com.br",9001,"brware","SQRT(pi)!=314","frontEnd");
-
-// var trySubscribe = setInterval(function(){
-//     if(mqttClient.isConnected()) {
-//         MQTT.subscribe("/stations/beacons/get",0);
-//         clearInterval(trySubscribe);
-//     }
-// },500);
---
+++ Software/start.js
@@ -0,0 +1,12 @@
+drawStations();
+
+drawTableHeader();
+
+MQTT.connect("brware.com.br",9001,"brware","SQRT(pi)!=314","frontEnd");
+
+var trySubscribe = setInterval(function(){
+    if(mqttClient.isConnected()) {
+        MQTT.subscribe("/stations/beacons/get",0);
+        clearInterval(trySubscribe);
+    }
+},500);
